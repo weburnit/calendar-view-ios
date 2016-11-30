de30200dbd0530d21616625457be66212eed0653
Phi Nguyen <phi.nguyen@bstarsolutions.com>
2016-11-30 18:09:47 +0700

    - Update SLCalendarView lib

--- SLCalendarView/SLCalendar.swift
+++ SLCalendarView/SLCalendar.swift
@@ -10,13 +10,23 @@ import UIKit
 
 public class SLCalendarView{
     
-    public static func performSegueToSLCalendarView(caller: UIViewController) {
+    public static func getBundleSLCalendarView() -> Bundle {
         let podBundle   = Bundle(for: SLCalendarView.self)
         let bundleURL   = podBundle.url(forResource: "SLCalendarView", withExtension: "bundle")
         let bundle      = Bundle(url: bundleURL!)!
+        return bundle
+    }
+    
+    public static func performSegueToSLCalendarView(caller: UIViewController) {
+        let bundle      = SLCalendarView.getBundleSLCalendarView()
         let storyboard  = UIStoryboard(name: "Calendar", bundle: bundle)
         let vc          = storyboard.instantiateInitialViewController()!
-        caller.present(vc, animated: true, completion: nil)
+        let navigationC = caller.navigationController
+        if navigationC != nil {
+            navigationC?.pushViewController(vc, animated: true)
+        }else{
+            caller.present(vc, animated: true, completion: nil)
+        }
     }
     
 }
