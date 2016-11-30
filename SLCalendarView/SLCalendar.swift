cc1a2c6b5781b82527c19e0a33ffedd1ef39ee77
Phi Nguyen <phi.nguyen@bstarsolutions.com>
2016-11-30 17:41:36 +0700

    - Minor change

---
+++ SLCalendarView/SLCalendar.swift
@@ -0,0 +1,13 @@
+//
+//  SLCalendar.swift
+//  CalendarView
+//
+//  Created by Nguyen Duc Phi on 11/30/16.
+//  Copyright © 2016 SweetPea. All rights reserved.
+//
+
+import UIKit
+
+class SLCalendar: NSObject {
+
+}
